e713e0c7f809c96230716995a9d5094fba8a9c7a
23ashutosh3 <60299983+23ashutosh3@users.noreply.github.com>
2021-09-25 19:11:17 +0530

    Update post.js

--- controllers/api/v1/post.js
+++ controllers/api/v1/post.js
@@ -1,6 +1,6 @@
 const Post = require("../../../models/postSchema");
 
-//add votes for specific answer
+// Authenticated  user can create post
 module.exports.userPost = async function(req, res) {
   try {
     console.log("hello1");
@@ -16,7 +16,7 @@ module.exports.userPost = async function(req, res) {
     res.status(500).json({ error: err.message });
   }
 };
-
+//Authenticated user can get all the post
 module.exports.getPost = async function(req, res) {
   const limitValue = req.query.limit || 2;
   const skipValue = req.query.skip || 0;
@@ -33,41 +33,50 @@ module.exports.getPost = async function(req, res) {
       });
     });
 };
-
+//Authenticated user can delete post
 module.exports.deletePost = async function(req, res) {
-  try {
-    let { post_id } = req.query;
 
-    const postData = await Post.findById({ _id: post_id });
+  try {
+    const postData = await Post.findById(req.params.id);
     if (!postData) {
-      return res.status(401).json({
-        err: "Bad request",
+      return res.json(400, {
+        message: "post not found",
       });
     }
 
-    const deleteUser = await Post.findByIdAndDelete({ _id: post_id });
-    res.json({ message: "success", deletePost });
+    postData.remove();
+
+    return res.json(200, {
+      message: "Post deleted",
+    });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.json(500, {
+      message: "internal error",
+    });
   }
 };
-
+//Authenticated user can update post
 module.exports.updatePost = async function(req, res) {
   try {
-    let { post_id } = req.query;
+    let { post_id } = req.params.id;
+    console.log("req.query",req.params.id);
 
-    const postData = await Post.findById({ _id: post_id });
+    const postData = await Post.findById(req.params.id);
+    console.log("postData", postData);
+
     if (!postData) {
       return res.status(401).json({
         err: "Bad request",
       });
     }
-    const { post, author, categories } = req.body;
+    const { post, author } = req.body;
     let obj = {};
     obj.post = post;
     obj.author = author;
+    console.log("obj", obj);
 
-    const savePost = await postData.save(obj);
+    const savePost = postData.save(obj);
+    console.log("savePost", savePost);
     res.json({
       message: "success",
       post,
