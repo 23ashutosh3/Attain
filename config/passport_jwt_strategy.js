105fb10f7c6cd29cd4b067390f35c0fcc1821fbf
23ashutosh3 <60299983+23ashutosh3@users.noreply.github.com>
2021-09-25 19:10:34 +0530

    Update passport_jwt_strategy.js

--- config/passport_jwt_strategy.js
+++ config/passport_jwt_strategy.js
@@ -1,34 +1,23 @@
 const passport = require("passport");
-const JWTStrategy = require("passport-jwt").Strategy;
-const ExtractJWT = require("passport-jwt").ExtractJwt;
-
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/userSchema");
-console.log("hello1");
-
-let opts = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "Hello123456",
-};
-
-console.log("hello1");
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "Hello123456";
+// opts.issuer = 'accounts.examplesoft.com';
+// opts.audience = 'yoursite.net';
 passport.use(
-  new JWTStrategy(opts, function(jwtPayLoad, done) {
-    console.log("hello4", jwtPayLoad._id);
-    User.findById(jwtPayLoad._id, function(err, user) {
+  new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function(err, user) {
       if (err) {
-        console.log("Error in finding user from JWT");
-        return;
+        return done(err, false);
       }
-
       if (user) {
-        console.log("hello3", user);
         return done(null, user);
       } else {
         return done(null, false);
       }
     });
-    console.log("hello2", user);
   })
 );
-
-module.exports = passport;
