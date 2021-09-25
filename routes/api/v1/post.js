79c70a51e938f4336a57b95322388dc4a9d70bd3
23ashutosh3 <60299983+23ashutosh3@users.noreply.github.com>
2021-09-25 19:13:33 +0530

    Update post.js

--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const userPost = require("../../../controllers/api/v1/post");
-const passport = require('passport');
-router.post("/userPost", userPost.userPost);
-router.get("/getPost", userPost.getPost);
-router.get("/delete", userPost.deletePost);
+const passport = require("passport");
+router.post(
+  "/userPost",
+  passport.authenticate("jwt", { session: false }),
+  userPost.userPost
+);
+router.get(
+  "/getPost",
+  passport.authenticate("jwt", { session: false }),
+  userPost.getPost
+);
+router.delete(
+  "/:id/delete",
+  passport.authenticate("jwt", { session: false }),
+  userPost.deletePost
+);
+router.put(
+  "/:id/update",
+  passport.authenticate("jwt", { session: false }),
+  userPost.updatePost
+);
 module.exports = router;
